1982639292beafa95452098c21cb8e0f2d6de333
lopun <ujin43255252@gmail.com>
2018-10-30 14:54:37 +0900

    Cloudinary Configured & tslint configuration changed

--- src/cloudinary.tsx
+++ src/cloudinary.tsx
@@ -1,3 +1,3 @@
 export const CLOUDINARY_API_KEY = "614533534772793";
 export const CLOUDINARY_UPLOAD_PRESET = "b0utibks";
-export const CLOUDINARY_ID = "dplj1ji7x";
+export const CLOUDINARY_ID = "qqv2mk01";
