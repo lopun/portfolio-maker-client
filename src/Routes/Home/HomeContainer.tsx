ad2fd928f8ecb9f2633cfab47595514d6df8daf1
lopun <ujin43255252@gmail.com>
2018-11-02 13:58:30 +0900

    Home Route added

---
+++ src/Routes/Home/HomeContainer.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import HomePresenter from "./HomePresenter";
+import { ALL_USERS } from "./HomeQueries";
+import { Query } from "react-apollo";
+import { toast } from "react-toastify";
+import { allUsers, userProfile } from "src/types/api";
+
+class AllUsersQuery extends Query<userProfile> {}
+
+class HomeContainer extends React.Component<any> {
+  public state = {
+    users: []
+  };
+
+  public render() {
+    const { handlePush } = this;
+    return (
+      <AllUsersQuery
+        query={ALL_USERS}
+        onCompleted={data => this.updateFields(data)}
+        fetchPolicy={"cache-and-network"}
+      >
+        {({ data, loading, error }) => {
+          return (
+            <HomePresenter
+              users={this.state.users}
+              loading={loading}
+              handlePush={handlePush}
+            />
+          );
+        }}
+      </AllUsersQuery>
+    );
+  }
+
+  public handlePush = user => {
+    const { history } = this.props;
+    console.log(this.props);
+    if (user) {
+      history.push({ pathname: `/users/${user.id}` });
+    } else {
+      history.push("/");
+    }
+  };
+
+  public updateFields = async (data: {} | allUsers) => {
+    if ("AllUsers" in data) {
+      const {
+        AllUsers: { ok, error, users }
+      } = data;
+      if (ok) {
+        if (users !== [] || users !== undefined) {
+          this.setState({
+            users
+          } as any);
+        }
+      } else if (error) {
+        toast.error(error);
+      }
+    }
+  };
+}
+
+export default HomeContainer;
---
+++ src/Routes/Home/HomeQueries.ts
@@ -0,0 +1,14 @@
+import { gql } from "apollo-boost";
+
+export const ALL_USERS = gql`
+  query allUsers {
+    AllUsers {
+      ok
+      error
+      users {
+        id
+        fullName
+      }
+    }
+  }
+`;
