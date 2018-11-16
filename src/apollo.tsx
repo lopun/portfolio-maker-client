afce21ef934fb37cc122577161254747cb1c2ca9
lopun <ujin43255252@gmail.com>
2018-11-16 11:04:22 +0900

    production url added

--- src/apollo.tsx
+++ src/apollo.tsx
@@ -48,7 +48,7 @@ const client = new ApolloClient({
   },
   uri: isDev
     ? "http://localhost:4000/graphql"
-    : "https://portfolio-maker-server.lopun.org/graphql"
+    : "https://portfolio-maker-server-aanmtfngos.now.sh/graphql"
 });
 
 export default client;
